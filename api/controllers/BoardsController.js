fcf82e851226415134edb0aff9b3e3466e6cffeb
shrikanth_kr <shrikanth_kr@yahoo.com>
2015-05-30 13:22:54 +0530

    initial commit

---
+++ api/controllers/BoardsController.js
@@ -0,0 +1,11 @@
+/**
+ * BoardsController
+ *
+ * @description :: Server-side logic for managing boards
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
--- tasks/register/compileAssets.js
+++ tasks/register/compileAssets.js
@@ -2,8 +2,8 @@ module.exports = function (grunt) {
 	grunt.registerTask('compileAssets', [
 		'clean:dev',
 		'jst:dev',
-		'less:dev',
 		'copy:dev',
+		'sass:dev',
 		'coffee:dev'
 	]);
 };
