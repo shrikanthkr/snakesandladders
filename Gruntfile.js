46120c64f4df7fc68231e9d9968f46ab46d6362d
shrikanth_kr <shrikanth_kr@yahoo.com>
2015-05-31 21:13:28 +0530

    revert

--- Gruntfile.js
+++ Gruntfile.js
@@ -77,5 +77,5 @@ module.exports = function(grunt) {
 	// Run task functions to configure Grunt.
 	invokeConfigFn(taskConfigurations);
 	invokeConfigFn(registerDefinitions);
-	grunt.loadNpmTasks('grunt-contrib-sass');
+
 };
