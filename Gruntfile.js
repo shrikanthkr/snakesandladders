f8b44530eabaaa2141f6dd82b5642d49799b9b4f
shrikanth_kr <shrikanth_kr@yahoo.com>
2015-06-01 22:32:27 +0530

    dummy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@
 
 module.exports = function(grunt) {
 	
-	
+	grunt.registerTask('heroku:production',['clean:dev']);
 	// Load the include-all library in order to require all of our grunt
 	// configurations and task registrations dynamically.
 	var includeAll;
