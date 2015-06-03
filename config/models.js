4ff53b544e0ff0a5ba80ced79817ae0fbcb5fcad
shrikanth_kr <shrikanth_kr@yahoo.com>
2015-06-03 07:25:04 +0530

    update models

--- config/models.js
+++ config/models.js
@@ -29,6 +29,7 @@
   ***************************************************************************/
   autoCreatedAt: true,
   autoUpdatedAt: true,
+  connection: 'postgresqlServer',
   migrate: 'safe',
 
 };
